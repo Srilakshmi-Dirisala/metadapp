db077bf32278824bfbd0d931443c3ccff38cdc2c
Srilakshmi <dirisalasrilakshmi@gmail.com>
2023-05-25 11:21:06 +0530

    added files

---
+++ database/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const sql = require("mssql");
+
+const config = {
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  options: {
+      trustedConnection: true,
+      encrypt: true,
+      enableArithAbort: true,
+      trustServerCertificate: true,
+  }
+}
+
+const poolPromise = new sql.ConnectionPool(config)
+  .connect()
+  .then(async (pool) => {
+    console.log('Connected to MSSQL')
+    return pool
+  })
+  .catch((err) => console.log('Database Connection Failed! Bad Config: ', err))
+
+module.exports = {
+    poolPromise
+}
---
+++ express/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const { endpoint } = require('../config');
+const { poolPromise } = require('../database');
+//const customerRoute = require('../src/routes/customer.route')
+
+module.exports = () => {
+  poolPromise;
+  const app = express();
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use((error, request, response, next) => {
+    if (error !== null) {
+      return response.json({ status: 401, message: "Invalid json", error: error });
+    }
+    next();
+  });
+
+  //app.use(endpoint,customerRoute)
+  return app;
+}
